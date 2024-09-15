3183d6efc5fe54bfe01ecd027fdbf46ff1a46e89
myrddral <thirdeye0@gmail.com>
2024-09-15 20:00:20 +0200

    updated base color

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import { Button } from '@/components/ui/button'
 
 export default function Page() {
   return (
-    <div className='flex-center h-1/2 gap-6'>
-      <p>MAIN CONTENT</p>
-      <Button>Hello</Button>
+    <div className='flex-center h-full gap-10'>
+      <div>
+        <h1 className='text-4xl font-bold'>
+          Learn to <span className='text-yellow-500'>code</span>.
+        </h1>
+        <h1 className='text-4xl font-bold'>
+          Build <span className='text-yellow-500'>projects</span>.
+        </h1>
+      </div>
+      <Button>Get Started</Button>
     </div>
   )
 }
