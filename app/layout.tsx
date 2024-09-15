36be30ffa1fc21737029c740ea2cfed847d5eb29
myrddral <thirdeye0@gmail.com>
2024-09-15 19:32:00 +0200

    added basic metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'react'
+import type { Metadata } from 'next'
 
 import Footer from '@/components/layout/footer'
 import Header from '@/components/layout/header'
@@ -6,11 +7,17 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { fontSans, fontDisplay, fontMono } from '@/lib/fonts'
 import '@/styles/globals.css'
 
+export const metadata: Metadata = {
+  title: 'Garuda Academy',
+  description: 'Garuda Academy',
+  keywords: 'garuda academy, programming, learning, education',
+}
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html
       lang='en'
-      className={`dark ${fontSans.variable} ${fontDisplay.variable} ${fontMono.variable}`}
+      className={`${fontSans.variable} ${fontDisplay.variable} ${fontMono.variable}`}
       suppressHydrationWarning
     >
       <body className='grid min-h-[100dvh] max-w-screen-2xl grid-rows-[auto_1fr_auto]'>
