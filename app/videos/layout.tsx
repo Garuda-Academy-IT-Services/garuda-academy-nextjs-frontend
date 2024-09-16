38f3944ca3e599c4f768c1af4d0f3ce2d5cc190b
myrddral <thirdeye0@gmail.com>
2024-09-16 23:22:28 +0200

    added breadcrumbs to /videos path

--- app/videos/layout.tsx
+++ app/videos/layout.tsx
@@ -1,3 +1,10 @@
+import { BreadcrumbsWrapper } from '@/components/breadcrumbs-wrapper'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className='h-full'>{children}</div>
+  return (
+    <div className='h-full p-2 md:p-6'>
+      <BreadcrumbsWrapper />
+      {children}
+    </div>
+  )
 }
---
+++ components/breadcrumbs-wrapper.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+import { Breadcrumbs } from './breadcrumbs'
+import { categories } from '@/lib/video-api/categories'
+
+export type Crumb = { href?: string; label: string }
+
+export function BreadcrumbsWrapper() {
+  const params = useSearchParams()
+  const category = params.get('category')
+
+  const getCategoryName = (id: string | null) => {
+    const cat = categories?.find((category) => category.id === Number(id))?.name
+    return cat || ''
+  }
+
+  const breadcrumbItems: Crumb[] = [
+    { href: '/', label: 'Főoldal' },
+    { href: '/videos', label: 'Videók' },
+    // Add other routes here, based on the current route path
+    { label: getCategoryName(category) },
+  ]
+
+  return (
+    <Suspense>
+      <Breadcrumbs items={breadcrumbItems} />
+    </Suspense>
+  )
+}
