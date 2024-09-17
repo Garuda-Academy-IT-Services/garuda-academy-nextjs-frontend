b44fcc961c0e3f6548efc5feba0c0e7992da1ef2
myrddral <thirdeye0@gmail.com>
2024-09-17 18:25:09 +0200

    implemented function to get video by id

---
+++ lib/video-api/get-video-by-id.ts
@@ -0,0 +1,13 @@
+import 'server-only'
+import type { Video } from '../types/video-api.types'
+
+export default async function getVideoById(id: string): Promise<Video> {
+  try {
+    const res = await fetch(`${process.env.API_URL}/videos/get/${id}`)
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error(error)
+    return {} as Video
+  }
+}
