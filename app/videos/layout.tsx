95a61a8d6bed945265b6e9cf3b05f323f0e3df4b
myrddral <thirdeye0@gmail.com>
2024-09-19 05:57:11 +0200

    minor semantic adjustment of layout

--- app/videos/layout.tsx
+++ app/videos/layout.tsx
@@ -3,11 +3,11 @@ import { Suspense } from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='h-full p-2 md:p-6'>
+    <section className='h-full p-2 md:p-6'>
       <Suspense>
         <BreadcrumbsWrapper />
       </Suspense>
       {children}
-    </div>
+    </section>
   )
 }
