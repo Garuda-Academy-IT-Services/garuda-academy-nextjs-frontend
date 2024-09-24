e64ee3252dbc6f7bd30cef3b2e9a58acd7676203
myrddral <thirdeye0@gmail.com>
2024-09-24 05:18:11 +0200

    profile page now displays user's email, name and avatar

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,16 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { auth } from '@/lib/auth'
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 export default async function Page() {
   const session = await auth()
+
+  if (!session) redirect('/')
+
+  const { user } = session
+
+  if (!user) throw new Error('User not found within session')
 
   return (
     <div>
@@ -14,14 +21,20 @@ export default async function Page() {
         </CardHeader>
         <CardContent className='flex gap-6'>
           <div className='h-auto w-36 overflow-clip rounded-lg'>
-            <Image src={session?.user?.image || ''} alt={session?.user?.name || ''} width={600} height={400} priority />
+            <Image
+              src={user.image || 'https://placehold.co/200x200@2x.png'}
+              alt={user.image ? `${user.name}'s profile picture` : 'No profile picture'}
+              width={600}
+              height={400}
+              priority
+            />
           </div>
           <div className='py-2'>
             <p className='text-sm'>Név</p>
-            <p className='font-semibold'>{session?.user?.name}</p>
+            <p className='font-semibold'>{user.name}</p>
             <br />
             <p className='text-sm'>Email</p>
-            <p className='font-semibold'>{session?.user?.email}</p>
+            <p className='font-semibold'>{user.email}</p>
           </div>
         </CardContent>
       </Card>
