1536f1e26d8868bb4a3e150cb76f9aa80f51ba00
myrddral <thirdeye0@gmail.com>
2024-11-17 20:01:33 +0100

    removed unnecessary check of user

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,8 +10,6 @@ export default async function Page() {
 
   const { user } = session
 
-  if (!user) throw new Error('User not found within session')
-
   return (
     <div>
       <Card className='bg-card/60 backdrop-blur'>
