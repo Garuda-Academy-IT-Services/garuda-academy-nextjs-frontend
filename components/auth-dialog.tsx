be52e6ea13cbba09c6cf005f5c451eef57fd763d
myrddral <thirdeye0@gmail.com>
2024-11-26 02:36:17 +0100

    added better handling of "action" param which controls the signin/signup dialog's state

--- components/auth-dialog.tsx
+++ components/auth-dialog.tsx
@@ -4,32 +4,40 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { Button } from './ui/button'
 import { Separator } from './ui/separator'
 import { LoginForm } from './login-form'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { SignUpForm } from './sign-up-form'
 
 export function AuthDialog({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
   const params = useSearchParams()
   const isSignUp = params.get('action') === 'signup'
 
+  const handleOpenChange = (open: boolean) => {
+    if (open) {
+      // Add signin action when dialog opens
+      const newParams = new URLSearchParams(params)
+      newParams.set('action', 'signin')
+      router.push(`?${newParams.toString()}`)
+    } else {
+      // Remove action param when dialog closes
+      const newParams = new URLSearchParams(params)
+      newParams.delete('action')
+      router.push(`?${newParams.toString()}`)
+    }
+  }
+
   return (
-    <Dialog>
+    <Dialog onOpenChange={handleOpenChange}>
       <DialogTrigger asChild>
         <Button variant='default' size={'sm'}>
           Bejelentkezés
         </Button>
       </DialogTrigger>
-      <DialogContent className='max-h-screen w-auto max-w-sm overflow-y-auto px-0 pt-0 mt-6'>
+      <DialogContent className='mt-6 max-h-screen w-auto max-w-sm overflow-y-auto px-0 pt-0'>
         <DialogHeader>
-          <div 
-            key={isSignUp ? 'signup-content' : 'login-content'}
-            className='space-y-0'
-          >
-            <DialogTitle className='sr-only'>
-              {isSignUp ? 'Regisztráció' : 'Bejelentkezés'}
-            </DialogTitle>
-            <DialogDescription className='sr-only'>
-              {isSignUp ? 'Regisztráció űrlap' : 'Bejelentkezés űrlap'}
-            </DialogDescription>
+          <div key={isSignUp ? 'signup-content' : 'login-content'} className='space-y-0'>
+            <DialogTitle className='sr-only'>{isSignUp ? 'Regisztráció' : 'Bejelentkezés'}</DialogTitle>
+            <DialogDescription className='sr-only'>{isSignUp ? 'Regisztráció űrlap' : 'Bejelentkezés űrlap'}</DialogDescription>
             {isSignUp ? <SignUpForm /> : <LoginForm />}
             <Separator />
             {children}
