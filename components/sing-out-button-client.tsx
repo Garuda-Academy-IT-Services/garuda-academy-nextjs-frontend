cfc8496e136e1319964a8e05200d2aba358dd2c9
myrddral <thirdeye0@gmail.com>
2024-12-28 18:55:14 +0100

    added debug logging of redirect URL

--- components/sing-out-button-client.tsx
+++ components/sing-out-button-client.tsx
@@ -1,11 +1,18 @@
 'use client'
 import { signOut } from 'next-auth/react'
 import { Button } from './ui/button'
+import { logger } from '@/lib/logger'
 
 export function SignOutButtonClient() {
   const protocol = window.location.protocol
   const hostName = window.location.hostname
   const redirectTo = `${protocol}//${hostName}`
+
+  logger.debug('SignOutButtonClient', {
+    protocol,
+    hostName,
+    redirectTo,
+  })
 
   return (
     <Button onClick={() => signOut({ redirectTo })} size={'sm'} variant={'ghost'}>
