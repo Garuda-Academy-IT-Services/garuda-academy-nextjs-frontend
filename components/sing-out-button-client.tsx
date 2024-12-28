5edb5b40c3c10fbc55af8ba65667192b5cf1b148
myrddral <thirdeye0@gmail.com>
2024-12-28 13:15:48 +0100

    fix: modified redirect url after logout and added query param removal after successful login

--- components/sing-out-button-client.tsx
+++ components/sing-out-button-client.tsx
@@ -3,8 +3,12 @@ import { signOut } from 'next-auth/react'
 import { Button } from './ui/button'
 
 export function SignOutButtonClient() {
+  const protocol = window.location.protocol
+  const hostName = window.location.hostname
+  const redirectTo = `${protocol}//${hostName}`
+
   return (
-    <Button onClick={() => signOut({ redirectTo: '/' })} size={'sm'} variant={'ghost'}>
+    <Button onClick={() => signOut({ redirectTo })} size={'sm'} variant={'ghost'}>
       Kijelentkezés
     </Button>
   )
